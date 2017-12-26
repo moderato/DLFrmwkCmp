3817663bc4f5607fa29759fcd3a277711c288200
moderato <moderato926@gmail.com>
2017-12-25 20:38:30 -0800

    minor changes

--- Keras.py
+++ Keras.py
@@ -19,7 +19,7 @@ dataset = "GT"
 root, trainImages, trainLabels, testImages, testLabels, class_num = DLHelper.getImageSets(root, resize_size, dataset=dataset, printing=True)
 x_train, x_valid, y_train, y_valid = ms.train_test_split(trainImages, trainLabels, test_size=0.2, random_state=542)
 
-epoch_num = 1
+epoch_num = 25
 batch_size = 64
 
 from keras.layers import Conv2D as keras_Conv
@@ -106,7 +106,7 @@ def constructCNN(layer_name_prefix, cnn_type="self"):
         keras_model.add(keras_MaxPooling(pool_size=(2, 2), name=layer_name_prefix+"pool1"))
         keras_model.add(keras_Conv(150, (4, 4), strides=(1, 1), activation="relu", name=layer_name_prefix+"conv2"))
         keras_model.add(keras_MaxPooling(pool_size=(2, 2), name=layer_name_prefix+"pool2"))
-        keras_model.add(keras_Conv(250, (3, 3), strides=(1, 1), activation="relu", input_shape=(resize_size[0], resize_size[1], 3), name=layer_name_prefix+"conv3"))
+        keras_model.add(keras_Conv(250, (3, 3), strides=(1, 1), activation="relu", name=layer_name_prefix+"conv3"))
         keras_model.add(keras_MaxPooling(pool_size=(2, 2), name=layer_name_prefix+"pool3"))
         keras_model.add(Flatten(name=layer_name_prefix+"flatten")) # An extra layer to flatten the previous layer in order to connect to fully connected layer
         keras_model.add(Dense(200, activation="relu", name=layer_name_prefix+"fc1"))
@@ -172,9 +172,9 @@ for b in backends:
     keras_cost = "categorical_crossentropy"
     keras_model.compile(loss=keras_cost, optimizer=keras_optimizer, metrics=["acc"])
 
-    checkpointer = ModelCheckpoint(filepath="{}/saved_models/keras_{}_{}_{}_weights.hdf5".format(root, b, device, dataset),
+    checkpointer = ModelCheckpoint(filepath="./saved_models/keras_{}_{}_{}_weights.hdf5".format(b, device, dataset),
                                        verbose=1, save_best_only=True)
-    losses = LossHistory("{}/saved_data/callback_data_keras_{}_{}_{}.h5".format(root, b, device, dataset), epoch_num, max_total_batch)
+    losses = LossHistory("./saved_data/callback_data_keras_{}_{}_{}.h5".format(b, device, dataset), epoch_num, max_total_batch)
 
     start = time.time()
     keras_model.fit(keras_train_x, keras_train_y,
@@ -182,7 +182,7 @@ for b in backends:
                   epochs=epoch_num, batch_size=batch_size, callbacks=[checkpointer, losses], verbose=1, shuffle=True)
     print("{} training finishes in {:.2f} seconds.".format(b, time.time() - start))
 
-    keras_model.load_weights("{}/saved_models/keras_{}_{}_{}_weights.hdf5".format(root, b, device, dataset)) # Load the best model (not necessary the latest one)
+    keras_model.load_weights("./saved_models/keras_{}_{}_{}_weights.hdf5".format(b, device, dataset)) # Load the best model (not necessary the latest one)
     keras_predictions = [np.argmax(keras_model.predict(np.expand_dims(feature, axis=0))) for feature in keras_test_x]
 
     # report test accuracy
@@ -192,6 +192,6 @@ for b in backends:
     print('{} test accuracy: {:.1f}%'.format(b, keras_test_accuracy))
 
     json_string = keras_model.to_json()
-    js = open("{}/saved_models/keras_{}_{}_{}_config.json".format(root, b, device, dataset), "a")
+    js = open("./saved_models/keras_{}_{}_{}_config.json".format(b, device, dataset), "w")
     js.write(json_string)
     js.close()
