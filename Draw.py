e54cba848d4c82416209c78b09b36f7e4d027411
ZhongyiLin <moderato@ZhongyiLindeMacBook-Pro.local>
2018-01-04 14:30:07 -0800

    Separate gpu and cpu drawing

--- Draw.py
+++ Draw.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
-import h5py, sys
+import h5py, sys, os
 # %matplotlib inline
 matplotlib.rcParams['figure.figsize'] = (10,8)
 
@@ -12,8 +12,20 @@ else:
 dataset = sys.argv[1]
 if dataset == "GT":
     root += "/GTSRB/try"
-model = 'resnet-56'
+model = sys.argv[2]
 data_path = root + "/saved_data/" + model
+devices = sys.argv[3:]
+
+pics_path = root
+if not os.path.exists(pics_path + "/pics/"):
+    pics_path += "/pics/"
+    os.makedirs(pics_path)
+    if not os.path.exists(pics_path + model):
+        pics_path += model
+        os.makedirs(pics_path)
+        for device in devices:
+            if not os.path.exists(pics_path + "/" + device):
+                os.makedirs(pics_path + "/" + device)
 
 gpu_backends = ["neon", "keras_tensorflow", "keras_theano", "cntk", "mxnet", "pytorch"]
 cpu_backends = ["neon", "neon_mkl", "keras_tensorflow", "keras_theano", "cntk", "mxnet", "pytorch"]
@@ -24,33 +36,31 @@ colors = {'neon': 'royalblue', 'neon_mkl': 'g', 'keras_tensorflow': 'r',\
 markers = {'neon': 'o', 'neon_mkl': 'x', 'keras_tensorflow': 'p',\
     'keras_theano': 'v', 'cntk': '+', 'mxnet': 's', 'pytorch': '*'}
 
-ylim_high = {'IDSIA': 4, 'self': 4, 'resnet-32': 2.5, 'resnet-56': 6.5}
-
-figs = [None] * 10
-axes = [None] * 10
-
-for i in range(0,2):
-    b = 'GPU' if i == 0 else 'CPU'
-    subplots_num = (2, 3) if b == 'GPU' else (3, 3)
-    figs[i*5+0] = plt.figure()
-    axes[i*5+0] = figs[i*5+0].add_subplot(1,1,1)
-    figs[i*5+0].suptitle("Training loss versus time ({})".format(b), y=0.94)
-    figs[i*5+1], axes[i*5+1] = plt.subplots(subplots_num[0], subplots_num[1])
-    figs[i*5+1].suptitle("Training and validation loss versus epoch ({})".format(b), y=0.94)
-
-    figs[i*5+2] = plt.figure()
-    axes[i*5+2] = figs[i*5+2].add_subplot(1,1,1)
-    figs[i*5+2].suptitle("Training accuracy versus time ({})".format(b), y=0.94)
-    figs[i*5+3] = plt.figure()
-    axes[i*5+3] = figs[i*5+3].add_subplot(1,1,1)
-    figs[i*5+3].suptitle("Validation accuracy versus time and Inference accuracy ({})".format(b), y=0.94)
-    figs[i*5+4], axes[i*5+4] = plt.subplots(subplots_num[0], subplots_num[1])
-    figs[i*5+4].suptitle("Training and validation accuracy versus epoch ({})".format(b), y=0.94)
+ylim_high = {'idsia': 4, 'self': 4, 'resnet-32': 2.5, 'resnet-56': 6.5}
 
 print("Model: {}".format(model))
-for n in range(0, 2):
-    backends = gpu_backends if n == 0 else cpu_backends
-    device = 'gpu' if n == 0 else 'cpu'
+for device in devices:
+    pics_path = root + "/pics/" + model + "/" + device
+    figs = [None] * 5
+    axes = [None] * 5
+
+    subplots_num = (2, 3) if device == 'gpu' else (3, 3)
+    figs[0] = plt.figure()
+    axes[0] = figs[0].add_subplot(1,1,1)
+    figs[0].suptitle("Training loss versus time ({})".format(device), y=0.94)
+    figs[1], axes[1] = plt.subplots(subplots_num[0], subplots_num[1])
+    figs[1].suptitle("Training and validation loss versus epoch ({})".format(device), y=0.94)
+
+    figs[2] = plt.figure()
+    axes[2] = figs[2].add_subplot(1,1,1)
+    figs[2].suptitle("Training accuracy versus time ({})".format(device), y=0.94)
+    figs[3] = plt.figure()
+    axes[3] = figs[3].add_subplot(1,1,1)
+    figs[3].suptitle("Validation accuracy versus time and Inference accuracy ({})".format(device), y=0.94)
+    figs[4], axes[4] = plt.subplots(subplots_num[0], subplots_num[1])
+    figs[4].suptitle("Training and validation accuracy versus epoch ({})".format(device), y=0.94)
+
+    backends = gpu_backends if device == "gpu" else cpu_backends
     infer_acc = []
     fastest = None
     print(device)
@@ -65,7 +75,8 @@ for n in range(0, 2):
         valid_acc_epoch = pd.DataFrame()
         train_epoch_mark = dict()
         
-        f = h5py.File(data_path+"/callback_data_{}_{}_{}.h5".format(b, device, dataset), "r")
+        print(data_path)
+        f = h5py.File(data_path+"/{}/callback_data_{}_{}.h5".format(device, b, dataset), "r")
         actual_length = f['.']['config'].attrs['total_minibatches']
         
         train_cost_batch['{}_loss'.format(b)] = pd.Series(f['.']['cost']['train'][()]).iloc[0:actual_length] # Training loss per batch
@@ -111,77 +122,77 @@ for n in range(0, 2):
                 fastest = (b, train_acc_epoch['{}_t'.format(b)].iloc[-1])
 
         # Avg training cost per epoch
-        axes[n*5+0].plot(train_cost_epoch['{}_t'.format(b)], train_cost_epoch['{}_loss'.format(b)],\
+        axes[0].plot(train_cost_epoch['{}_t'.format(b)], train_cost_epoch['{}_loss'.format(b)],\
             marker=markers[b], color=colors[b])
 
         # Avg training cost vs valid cost per epoch
-        axes[n*5+1][int(i/3)][i%3].plot(range(len(train_epoch_mark['{}_mark'.format(b)])), \
+        axes[1][int(i/3)][i%3].plot(range(len(train_epoch_mark['{}_mark'.format(b)])), \
             train_cost_epoch['{}_loss'.format(b)], label='{}_t'.format(b), marker=markers["neon"])
-        axes[n*5+1][int(i/3)][i%3].plot(range(len(train_epoch_mark['{}_mark'.format(b)])), \
-            valid_cost_epoch['{}_loss'.format(b)], label='{}_v'.format(b), marker=markers["neon_mkl"])
-        axes[n*5+1][int(i/3)][i%3].legend(loc=4)
-        axes[n*5+1][int(i/3)][i%3].yaxis.grid(linestyle='dashdot')
-        axes[n*5+1][int(i/3)][i%3].set_xlabel('Epoch')
-        axes[n*5+1][int(i/3)][i%3].set_ylabel('Loss')
+        # axes[1][int(i/3)][i%3].plot(range(len(train_epoch_mark['{}_mark'.format(b)])), \
+        #     valid_cost_epoch['{}_loss'.format(b)], label='{}_v'.format(b), marker=markers["neon_mkl"])
+        axes[1][int(i/3)][i%3].legend(loc=4)
+        axes[1][int(i/3)][i%3].yaxis.grid(linestyle='dashdot')
+        axes[1][int(i/3)][i%3].set_xlabel('Epoch')
+        axes[1][int(i/3)][i%3].set_ylabel('Loss')
         
         # Avg training acc per epoch
-        axes[n*5+2].plot(train_acc_epoch['{}_t'.format(b)], -1*train_acc_epoch['{}_acc'.format(b)]+101,\
+        axes[2].plot(train_acc_epoch['{}_t'.format(b)], -1*train_acc_epoch['{}_acc'.format(b)]+101,\
             marker=markers[b], color=colors[b])
         
         # Valid acc per epoch
-        axes[n*5+3].plot(train_acc_epoch['{}_t'.format(b)], -1*valid_acc_epoch['{}_acc'.format(b)]+101,\
+        axes[3].plot(train_acc_epoch['{}_t'.format(b)], -1*valid_acc_epoch['{}_acc'.format(b)]+101,\
             marker=markers[b], color=colors[b])
         
         # Avg training acc vs valid acc per epoch
-        axes[n*5+4][int(i/3)][i%3].plot(range(len(train_epoch_mark['{}_mark'.format(b)])), \
+        axes[4][int(i/3)][i%3].plot(range(len(train_epoch_mark['{}_mark'.format(b)])), \
             train_acc_epoch['{}_acc'.format(b)], label='{}_t'.format(b), marker=markers["neon"])
-        axes[n*5+4][int(i/3)][i%3].plot(range(len(train_epoch_mark['{}_mark'.format(b)])), \
-            valid_acc_epoch['{}_acc'.format(b)], label='{}_v'.format(b), marker=markers["neon_mkl"])
-        axes[n*5+4][int(i/3)][i%3].legend(loc=4)
-        axes[n*5+4][int(i/3)][i%3].yaxis.grid(linestyle='dashdot')
-        axes[n*5+4][int(i/3)][i%3].set_xlabel('Epoch')
-        axes[n*5+4][int(i/3)][i%3].set_ylabel('Accuracy (%)')
+        # axes[4][int(i/3)][i%3].plot(range(len(train_epoch_mark['{}_mark'.format(b)])), \
+        #     valid_acc_epoch['{}_acc'.format(b)], label='{}_v'.format(b), marker=markers["neon_mkl"])
+        axes[4][int(i/3)][i%3].legend(loc=4)
+        axes[4][int(i/3)][i%3].yaxis.grid(linestyle='dashdot')
+        axes[4][int(i/3)][i%3].set_xlabel('Epoch')
+        axes[4][int(i/3)][i%3].set_ylabel('Accuracy (%)')
         
         f.close()
 
     print('\n')
 
-    axes[n*5+0].legend(loc=4)
-    axes[n*5+0].set_ylim((-0.1,ylim_high[model]))
-    axes[n*5+0].yaxis.grid(linestyle='dashdot')
-    axes[n*5+0].set_xlabel('Time (s)')
-    axes[n*5+0].set_ylabel('Loss')
+    axes[0].legend(loc=4)
+    axes[0].set_ylim((-0.1,ylim_high[model]))
+    axes[0].yaxis.grid(linestyle='dashdot')
+    axes[0].set_xlabel('Time (s)')
+    axes[0].set_ylabel('Loss')
 
-    handles, labels = axes[n*5+2].get_legend_handles_labels()
-    axes[n*5+2].legend(handles, [label[0:label.rfind('_')] for label in labels], loc=4)
-    axes[n*5+2].axvline(fastest[1], linestyle='dashed', color='#777777')
-    axes[n*5+2].text(fastest[1]*1.01, 12, "First Finished Training: " + fastest[0], size=12)
-    axes[n*5+2].set_ylim((0.9,101))
-    axes[n*5+2].set_yscale('log')
-    axes[n*5+2].set_yticks([101-num for num in (list(range(0,11,1)) + list(range(11,91,10)) + list(range(91,101,1)))])
-    axes[n*5+2].set_yticklabels(([''] * 11 + list(range(20,100,10)) + list(range(91,101,1))))
-    axes[n*5+2].invert_yaxis()
-    axes[n*5+2].yaxis.grid(linestyle='dashdot')
-    axes[n*5+2].set_xlabel('Time (s)')
-    axes[n*5+2].set_ylabel('Training Accuracy (%)')
+    handles, labels = axes[2].get_legend_handles_labels()
+    axes[2].legend(handles, [label[0:label.rfind('_')] for label in labels], loc=4)
+    axes[2].axvline(fastest[1], linestyle='dashed', color='#777777')
+    axes[2].text(fastest[1]*1.01, 12, "First Finished Training: " + fastest[0], size=12)
+    axes[2].set_ylim((0.9,101))
+    axes[2].set_yscale('log')
+    axes[2].set_yticks([101-num for num in (list(range(0,11,1)) + list(range(11,91,10)) + list(range(91,101,1)))])
+    axes[2].set_yticklabels(([''] * 11 + list(range(20,100,10)) + list(range(91,101,1))))
+    axes[2].invert_yaxis()
+    axes[2].yaxis.grid(linestyle='dashdot')
+    axes[2].set_xlabel('Time (s)')
+    axes[2].set_ylabel('Training Accuracy (%)')
 
-    handles, labels = axes[n*5+3].get_legend_handles_labels()
-    axes[n*5+3].legend(handles, [label[0:label.rfind('_')] for label in labels], loc=4)
-    axes[n*5+3].axvline(fastest[1], linestyle='dashed', color='#777777')
-    axes[n*5+3].text(fastest[1]*1.01, 12, "First Finished Training: " + fastest[0], size=12)
-    axes[n*5+3].set_ylim((0.9,101))
-    axes[n*5+3].set_yscale('log')
-    axes[n*5+3].set_yticks([101-num for num in (list(range(0,11,1)) + list(range(11,91,10)) + list(range(91,101,1)))])
-    axes[n*5+3].set_yticklabels(([''] * 11 + list(range(20,100,10)) + list(range(91,101,1))))
-    axes[n*5+3].invert_yaxis()
-    axes[n*5+3].yaxis.grid(linestyle='dashdot')
-    axes[n*5+3].set_xlabel('Time (s)')
-    axes[n*5+3].set_ylabel('Validation Accuracy (%)')
+    handles, labels = axes[3].get_legend_handles_labels()
+    axes[3].legend(handles, [label[0:label.rfind('_')] for label in labels], loc=4)
+    axes[3].axvline(fastest[1], linestyle='dashed', color='#777777')
+    axes[3].text(fastest[1]*1.01, 12, "First Finished Training: " + fastest[0], size=12)
+    axes[3].set_ylim((0.9,101))
+    axes[3].set_yscale('log')
+    axes[3].set_yticks([101-num for num in (list(range(0,11,1)) + list(range(11,91,10)) + list(range(91,101,1)))])
+    axes[3].set_yticklabels(([''] * 11 + list(range(20,100,10)) + list(range(91,101,1))))
+    axes[3].invert_yaxis()
+    axes[3].yaxis.grid(linestyle='dashdot')
+    axes[3].set_xlabel('Time (s)')
+    axes[3].set_ylabel('Validation Accuracy (%)')
 
-    figs[n*5+0].savefig(root+"/pics/{}/{}_train_loss_versus_time.png".format(model, device), dpi=figs[n*5+0].dpi)
-    figs[n*5+1].savefig(root+"/pics/{}/{}_train_and_valid_loss_versus_epoch.png".format(model, device), dpi=figs[n*5+1].dpi)
-    figs[n*5+2].savefig(root+"/pics/{}/{}_train_acc_versus_time.png".format(model, device), dpi=figs[n*5+2].dpi)
-    figs[n*5+3].savefig(root+"/pics/{}/{}_valid_acc_versus_time.png".format(model, device), dpi=figs[n*5+3].dpi)
-    figs[n*5+4].savefig(root+"/pics/{}/{}_train_and_valid_acc_versus_epoch.png".format(model, device), dpi=figs[n*5+4].dpi)
+    figs[0].savefig(pics_path+"/train_loss_versus_time.png".format(model, device), dpi=figs[0].dpi)
+    figs[1].savefig(pics_path+"/train_and_valid_loss_versus_epoch.png".format(model, device), dpi=figs[1].dpi)
+    figs[2].savefig(pics_path+"/train_acc_versus_time.png".format(model, device), dpi=figs[2].dpi)
+    figs[3].savefig(pics_path+"/valid_acc_versus_time.png".format(model, device), dpi=figs[3].dpi)
+    figs[4].savefig(pics_path+"/train_and_valid_acc_versus_epoch.png".format(model, device), dpi=figs[4].dpi)
     
-plt.show()
+    plt.show()
